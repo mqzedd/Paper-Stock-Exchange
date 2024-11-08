4c98a9e2aef24cc67e052d49f7c354fc40f46133
Mqzed1337 <153781300+Mqzed1337@users.noreply.github.com>
2024-11-09 03:00:08 +1030

    chore: add boilerplate for login and db systems

---
+++ database.py
@@ -0,0 +1,21 @@
+from bcrypt import checkpw
+
+
+def fetch_data(userid):
+    pass
+
+
+def update_data(userid, data):
+    pass
+
+
+def fetch_hash(username):
+    pass
+
+
+def register_user(username, hash):
+    pass
+
+
+# database has
+# id | username | password_hash | salt | data
---
+++ login.py
@@ -0,0 +1,16 @@
+import database
+import cryptography
+import bcrypt
+from database import fetch_hash
+
+hash = bcrypt.hashpw(b"password", bcrypt.gensalt())
+
+
+def validate_pwd(username, password):
+    return bcrypt.checkpw(password, fetch_hash(username))
+
+
+def login(username, password):
+    if validate_pwd(username, password):
+        return True
+    return False
