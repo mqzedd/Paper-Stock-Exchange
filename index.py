3b56ac74e6817298052c1761ec7c3db68a2a44c4
Mqzed1337 <153781300+Mqzed1337@users.noreply.github.com>
2024-11-09 00:47:55 +1030

    refactor: optimise stock selling function

--- index.py
+++ index.py
@@ -4,16 +4,23 @@ import requests
 def init():
     global portfolio
     global balance
+    # get portfolio and balance from database
     portfolio = {}  # stock_id: [quantity, average_price]
     balance = 0  # in dollars (USD)?
+    price_cache = {}  # stock_id: price
 
 
 def price(stock_id):
+    # get price of stock using API
     return 100
 
 
 def update_portfolio():
-    #
+    global price_cache
+    for stock_id in portfolio:
+        price_cache[stock_id] = price(stock_id)
+    # find price of every stock in portfolio - cache in a dictionary
+    # determine total value of portfolio
     pass
 
 
@@ -40,14 +47,15 @@ def buy_stock(stock_id, cost):
 
 def sell_stock(stock_id, cost=all):
     global portfolio
+    portfolio_quantity = portfolio[stock_id][0]
     if stock_id not in portfolio:
         return False
     if cost == all:
-        cost = price(stock_id) * portfolio[stock_id][0]
-    if cost > price(stock_id) * portfolio[stock_id][0]:
+        cost = price(stock_id) * portfolio_quantity
+    if cost > price(stock_id) * portfolio_quantity:
         return False
     quantity = cost / price(stock_id)
-    if quantity == portfolio[stock_id][0]:
+    if quantity == portfolio_quantity:
         balance += cost
         del portfolio[stock_id]
     else:
