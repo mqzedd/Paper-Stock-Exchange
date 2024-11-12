a8d9e709f00891c21c8f0b12a8599ccecf136ccd
Mqzed1337 <153781300+Mqzed1337@users.noreply.github.com>
2024-11-12 14:50:27 +1030

    feat: implement alphavantage API

--- stock.py
+++ stock.py
@@ -1,18 +1,45 @@
+# might need to switch from yfinance as the API is slower
 import requests
 import yfinance as yf
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+polygon_api_key = os.getenv("POLY_KEY")
+alphavantage_api_key = os.getenv("ALPHA_KEY")
 
 
 def init():
     global portfolio
     global balance
     global price_cache
+    load_dotenv()
+    polygon_api_key = os.getenv("POLY_KEY")
+    alphavantage_api_key = os.getenv("ALPHA_KEY")
+
     # get portfolio and balance from database
     portfolio = {}  # stock_id: [quantity, average_price]
     balance = 0  # in dollars (USD)?
     price_cache = {}  # stock_id: price
 
 
+init()
+
+
 # portfolio graphs should be YTD
+def generate_graph(stock_id, time_period="YTD"):
+    # https://api.polygon.io/v2/aggs/ticker/AAPL/prev?adjusted=true&apiKey=
+    url = f"https://api.polygon.io/v2/aggs/ticker/{stock_id}/prev?adjusted=true&apiKey={polygon_api_key}"
+    r = requests.get(url)
+    data = r.json()
+    return data
+
+
+def portfolio_graph(user_id):
+    # take all the stocks in portfolio, create a list for each day in the last year,
+    # # for each stock add their value  for each day to the list,
+    # send data to frontend and generate chard
+    pass
 
 
 # https://rapidapi.com/apidojo/api/yahoo-finance1
@@ -26,10 +53,13 @@ def price(stock_id):  # get price of stock using API, caching it for later use
 
 
 def search(search_term):
-    results = []
-    return results
+    url = f"https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords={search_term}&apikey={alphavantage_api_key}"
+    r = requests.get(url)
+    data = r.json()
+    return data
 
 
+# pd dataframe
 def update_portfolio():
     global price_cache
     portflio_string = " ".join(portfolio.keys()).lower()
