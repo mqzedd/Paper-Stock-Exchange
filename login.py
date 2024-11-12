5e48d3fb737bfc4e92a2dfbcfbdea05a38cf2ceb
Mqzed1337 <153781300+Mqzed1337@users.noreply.github.com>
2024-11-12 14:49:52 +1030

    chore: format login.py to PEP-8

--- login.py
+++ login.py
@@ -4,20 +4,23 @@ import bcrypt
 from database import fetch_hash
 
 
-
 def register(username, password):
-    hash = bcrypt.hashpw(password, bcrypt.gensalt()) #uses bcrypt to hash the password
-    if database.register_user(username, hash): #registers the user in the database
+    hash = bcrypt.hashpw(password, bcrypt.gensalt())  # uses bcrypt to hash the password
+    if database.register_user(username, hash):  # registers the user in the database
         return True
     return False
 
 
-def login(username, password): #fetch login data returns a tuple with id and password hash
-    if bcrypt.checkpw(password, fetch_login_data(username)[1]): #uses bcrypt to check if the password is correct
+def login(
+    username, password
+):  # fetch login data returns a tuple with id and password hash
+    if bcrypt.checkpw(
+        password, database.fetch_login_data(username)[1]
+    ):  # uses bcrypt to check if the password is correct
         # do authentication
         # get user data
         return True
     return False
 
 
-#todo implement authentication
+# todo implement authentication
