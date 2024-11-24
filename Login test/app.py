de6d4a1cd63aabfa91f48da19e113ba12671e6d4
Mqzed1337 <153781300+Mqzed1337@users.noreply.github.com>
2024-11-24 11:46:51 +1030

    feat: implemented login system and session persistence

--- Login test/app.py
+++ Login test/app.py
@@ -1,8 +1,7 @@
 import database
-import login
 import flask
 from flask import request, render_template
-
+import login as lg
 from dotenv import load_dotenv
 import os
 
@@ -20,18 +19,38 @@ app.secret_key = "your_secret_key"  # Keep this secret and secure
 
 @app.route("/")
 def index():
+    if "user_id" in session:
+        return render_template("index.html", user_id=session["user_id"])
     return render_template("index.html")
 
 
 @app.route("/login", methods=["POST", "GET"])
 def login():
     if request.method == "POST":
-        user_id = request.form.get("user_id")
-        if user_id:
+
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        if lg.login(username, password) != None:
+            user_id = lg.login(username, password)
             session["user_id"] = user_id  # Set user_id in session
             return redirect(url_for("index"))
         else:
-            return "Login failed", 401
+            return render_template("login.html", additional_message="Invalid login")
+    return render_template("login.html")
+
+
+@app.route("/register", methods=["POST", "GET"])
+def register():
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        if lg.register(username, password):
+            return render_template("login.html")
+        else:
+            return render_template(
+                "login.html", additional_message="Username already taken"
+            )
     return render_template("login.html")
 
 
