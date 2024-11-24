7f011c116486ad4d61828f3791e983a58128005d
Mqzed1337 <153781300+Mqzed1337@users.noreply.github.com>
2024-11-24 11:40:45 +1030

    fix: fix functions using wrong encoding

--- Login test/login.py
+++ Login test/login.py
@@ -5,22 +5,33 @@ from database import fetch_login_data
 
 
 def register(username, password):
-    hash = bcrypt.hashpw(password, bcrypt.gensalt())  # uses bcrypt to hash the password
+    hash = bcrypt.hashpw(
+        password.encode("utf-8"), bcrypt.gensalt()
+    )  # uses bcrypt to hash the password
     if database.register_user(username, hash):  # registers the user in the database
         return True
     return False
 
 
-def login(
-    username, password
-):  # fetch login data returns a tuple with id and password hash
-    if bcrypt.checkpw(
-        password, database.fetch_login_data(username)[1]
-    ):  # uses bcrypt to check if the password is correct
+def login(username, password):
+    # fetch login data returns user_id
+
+    login_data = database.fetch_login_data(username)
+    if not login_data:
+        return None
+    if bcrypt.checkpw(password.encode("utf-8"), login_data[1]):
         # do authentication
         # get user data
-        return True
-    return False
+        return login_data[0]
+    return None
+
+
+if __name__ == "__main__":
+    print("START")
+    print(login("test", "test") != None)
+    print(register("test", "test") == False)
+    print(database.fetch_login_data("test"))
+    print(login("test", "test"))
 
 
 # todo implement authentication
