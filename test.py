04a098abb9fef59cf0f057ab8dd8ebcfe6dc8cd0
mqzedd <153781300+mqzedd@users.noreply.github.com>
2024-11-26 05:37:32 +1030

    chore: update gitignore to remove test.py

--- test.py
+++ test.py
@@ -19,8 +19,27 @@ end = time.time()
 print(f"Time taken: {end - start} seconds")
 
 # Check if the request was successful
+
+# output  = {
+#    "Global Quote": {
+#        "01. symbol": "IBM",
+#        "02. open": "223.3500",
+#        "03. high": "227.2000",
+#        "04. low": "220.8900",
+#        "05. price": "222.9700",
+#        "06. volume": "5320740",
+#        "07. latest trading day": "2024-11-22",
+#        "08. previous close": "222.4000",
+#        "09. change": "0.5700",
+#        "10. change percent": "0.2563%"
+#    }
+# }
 if response.status_code == 200:
-    # Print the JSON response
-    print(response.json())
+    data = response.json()
+    price = data.get("Global Quote", {}).get("05. price")
+    if price:
+        print(f"The price of AAPL is: {price}")
+    else:
+        print("Price not found in the response.")
 else:
     print(f"Request failed with status code {response.status_code}")
